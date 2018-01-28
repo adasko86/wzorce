3a8dfe1479f9f4d75fb0762557027b34a50475ec
Adam <adasko.86@gmail.com>
2018-01-28 11:45:24 +0100

    Dodano wzorzec:
    Monostate

---
+++ Monostate/Monostate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monostate
+{
+    public class Monostate
+    {
+        private static int itsX = 0;
+        public Monostate() { }
+        public void setX(int x)
+        {
+            itsX = x;
+        }
+        public int getX()
+        {
+            return itsX;
+        }
+
+        public override string ToString()
+        {
+            return "Liczna wynosi : " + getX();
+        }
+    }
+}
---
+++ Monostate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monostate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monostate m1 = new Monostate();
+            Monostate m2 = new Monostate();
+
+            m1.setX(9);
+
+            Console.WriteLine("M1: " + m1.ToString());
+            Console.WriteLine("M2: " + m1.ToString());
+
+            m1.setX(22);
+
+            Console.WriteLine("M1: " + m1.ToString());
+            Console.WriteLine("M2: " + m1.ToString());
+
+            Console.Read();
+        }
+    }
+}
