34a23da419b6fea68f0c5dbe154136070f9ab73b
Adam <adasko.86@gmail.com>
2018-03-03 19:33:21 +0100

    Dodano wzorzec Obserwator

---
+++ Observer/Mezczyzna.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class Mezczyzna : Observable, Observator
+    {
+        String name;
+        private List<Observator> observators = new List<Observator>();
+
+        public Mezczyzna(String name)
+        {
+            this.name = name;
+        }
+
+        public void update(string changeFrom, string changeTo)
+        {
+            Console.WriteLine(name + " wlasnie zauwazyl, ze Kobieta zmienila " + changeFrom + " na " + changeTo + ".");
+        }
+
+        public void addObservator(Observator obj)
+        {
+            if (obj != null)
+                observators.Add(obj);
+        }
+
+        public void deleteObservator(Observator obj)
+        {
+            this.observators.Remove(obj);
+        }
+
+        public void notifyObservator()
+        {
+            foreach (Observator ob in observators)
+                ob.update("bluzke", "koszule");
+        }
+
+        public void changeBlouseToShirt()
+        {
+            Console.WriteLine(name + " zmienia bluzke na koszule.");
+            notifyObservator();
+        }
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kobieta ob1 = new Kobieta("Aga");
+            Mezczyzna ob2 = new Mezczyzna("Mateusz");
+            Mezczyzna ob3 = new Mezczyzna("Lukasz");
+            //========================
+            ob1.addObservator(ob2);
+            ob1.addObservator(ob3);
+            ob1.changeTrousersToSkirt();
+            //========================
+            ob2.addObservator(ob1);
+            ob2.changeBlouseToShirt();
+            Console.Read();
+        }
+    }
+}
