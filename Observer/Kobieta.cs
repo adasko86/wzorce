ef6bae6d3316f440332ffb6a978792e428b2c1c8
Adam Światek <30856387+adasko86@users.noreply.github.com>
2018-03-03 19:36:54 +0100

    Add files via upload

---
+++ Observer/Kobieta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class Kobieta : Observator, Observable
+    {
+        List<Observator> observators = new List<Observator>();
+        private string name = null;
+        public Kobieta(String _name)
+        {
+            name = _name;
+        }
+
+        public void changeTrousersToSkirt()
+        {
+            Console.WriteLine(name + " zmienia spodnie na spodniczke :) ");
+            notifyObservator();
+        }
+
+        public void addObservator(Observator obj)
+        {
+            if (obj != null && !observators.Contains(obj))
+                observators.Add(obj);
+        }
+
+        public void deleteObservator(Observator obj)
+        {
+            if (obj != null && observators.Contains(obj))
+                observators.Remove(obj);
+        }
+
+        public void notifyObservator()
+        {
+            foreach (Observator observator in observators)
+                observator.update("spodnie", "spodniczke");
+        }
+
+        public void update(string changeFrom, string changeTo)
+        {
+            Console.WriteLine(name + " zauwaza ze ktos zmienil " + changeFrom + " na " + changeTo + ", mrr.. :)");
+        }
+    }
+}
---
+++ Observer/Observable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public interface Observable {
+
+    void addObservator(Observator obj);
+
+    void deleteObservator(Observator obj);
+
+    void notifyObservator();
+}
+}
