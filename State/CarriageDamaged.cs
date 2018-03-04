4caa80cb97af2f3fb4fcb5527a83d779863544dc
Adam Światek <30856387+adasko86@users.noreply.github.com>
2018-03-04 11:15:57 +0100

    Add files via upload

---
+++ State/CarriageDamaged.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class CarriageDamaged : CarriageState
+    {
+        public void reportPosition(Carriage carriage)
+        {
+            Console.WriteLine(String.Format("Wagon: {0} jest w naprawie", carriage.getNumber()));
+        }
+    }
+}
---
+++ State/CarriageUnavailable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class CarriageUnavailable : CarriageState
+    {
+        public void reportPosition(Carriage carriage) {
+		Console.WriteLine(String.Format("Wagon: {0} jest niedostepny - w trakcie realizacji przewozu", carriage.getNumber()));
+	}
+    }
+}
